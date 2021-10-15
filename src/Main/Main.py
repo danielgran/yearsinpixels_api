32090f2c2a2040d1f180e0ab19313551fa5cff55
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-10-15 21:32:57 +0200

    Refactor

--- src/Main/Main.py
+++ src/Main/Main.py
@@ -1,5 +1,3 @@
-from Entity.User import User
-
 from src.EndPoint.EndPoint import EndPoint
 from src.EndPoint.SocketStrategy.FlaskWebStrategy import FlaskWebStrategy
 from src.Main.ConcreteFactory import ConcreteFactory
@@ -7,27 +5,19 @@ from src.RequestQueue.RequestQueue import RequestQueue
 
 
 def main():
-    fac = ConcreteFactory()
-    webhost = fac.CreateWebHost("localhost")
+    factory = ConcreteFactory()
+    webhost = factory.CreateWebHost("localhost")
     setup_webhost(webhost)
-
-
-
     webhost.run()
-
-
 
 def setup_webhost(webhost):
     expose_strategy = FlaskWebStrategy()
-
     request_queue = RequestQueue()
-
     webhost.setup_expose_strategy(expose_strategy)
 
     test_endpoint = EndPoint("/test")
     test_endpoint.set_request_queue(request_queue=request_queue)
     webhost.add_endpoint(test_endpoint)
-
 
 if __name__ == '__main__':
     main()
--- test/Main/test_Main.py
+++ test/Main/test_Main.py
@@ -5,7 +5,8 @@ from src.Main import Main
 
 class MainTest(unittest.TestCase):
 
-
     def test_isthere(self):
         self.assertTrue(Main)
 
+    def test_main(self):
+        self.assertIsNotNone(Main.main)
