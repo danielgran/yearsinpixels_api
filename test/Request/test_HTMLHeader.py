d08990cac1d10554a5b8e2dcc8c42b72fbe8c721
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-10-15 21:42:39 +0200

    Refactor

--- test/Request/test_HTMLHeader.py
+++ test/Request/test_HTMLHeader.py
@@ -4,28 +4,25 @@ from src.Request.HTMLHeader import HTMLHeader
 
 
 class test_HTMLHeader(unittest.TestCase):
+    def setUp(self):
+       self.header = HTMLHeader()
+       self.header.add_item(("some", "header"))
 
     def test_is_there(self):
-        self.assertIsNotNone(HTMLHeader)
+        self.assertIsNotNone(HTMLHeader(None))
 
     def test_add_item(self):
-        header = HTMLHeader()
-        header.add_item(("some", "thing"))
-        self.assertRaises(Exception, header.add_item, ("some", "as"))
-        self.assertEqual(header.get_content("some"), "thing")
+        self.assertRaises(Exception, self.header.add_item, ("some", "no_thing"))
+        self.assertEqual(self.header.get_content("some"), "header")
 
     def test_remove_item(self):
-        header = HTMLHeader()
-        header.add_item(("some", "thing"))
-        header.remove_item("some")
-        self.assertRaises(Exception, header.remove_item, "not_there")
+        self.header.remove_item("some")
+        self.assertRaises(Exception, self.header.remove_item, "some")
 
     def test_get_content(self):
-        header = HTMLHeader()
-        header.add_item(("some", "header"))
-        self.assertEqual(header.get_content("some"), "header")
+        self.assertEqual(self.header.get_content("some"), "header")
 
     def test_new_from_dict(self):
         header_mock = {'Host': 'localhost:8080', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Upgrade-Insecure-Requests': '1', 'Cookie': 'Webstorm-ef1f44d4=7fd8ddd3-93b4-4beb-bdea-b421d50c90ed', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15', 'Accept-Language': 'en-gb', 'Accept-Encoding': 'gzip, deflate', 'Connection': 'keep-alive'}
-        header = HTMLHeader(header_mock)
-        self.assertEqual(header_mock, header.items)
+        self.header = HTMLHeader(header_mock)
+        self.assertEqual(header_mock, self.header.items)
