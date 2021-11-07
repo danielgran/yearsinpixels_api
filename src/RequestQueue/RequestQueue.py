945dee2631488388c8d3bfec7c2b470f948ca1b5
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-11-07 17:02:23 +0100

    Added: Request Processors in RequestQueue

--- src/RequestQueue/RequestQueue.py
+++ src/RequestQueue/RequestQueue.py
@@ -5,6 +5,7 @@ class RequestQueue:
     def __init__(self):
         self.ingoing_queue = dict()
         self.outgoing_queue = dict()
+        self.processors = dict()
 
     def __len__(self):
         return len(self.ingoing_queue)
@@ -12,4 +13,12 @@ class RequestQueue:
     def add_incoming_request(self, incoming_request):
         guid = str(uuid.uuid4())
         self.ingoing_queue[guid] = incoming_request
+        self.notify_processor(incoming_request)
         return guid # todo would it not be better to have it return an request object to then await some state in the webstrategy to return?
+
+    def reqister_processor(self, path, processor):
+        self.processors[path] = processor
+
+    def notify_processor(self, request):
+        if (self.processors.get(request.path)):
+            self.processors.get(request.path).process(request)
--- test/RequestQueue/test_RequestQueue.py
+++ test/RequestQueue/test_RequestQueue.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest.mock import MagicMock
 
 from src.Request.Request import Request
+from src.RequestProcessor.DataProcessor.GraphQLProcessor.GraphQLProcessor import GraphQLProcessor
 from src.RequestQueue.RequestQueue import RequestQueue
 
 
@@ -14,3 +16,20 @@ class RequestQueueTest(unittest.TestCase):
     def test_add_ingoing_request(self):
         id = self.requestQueue.add_incoming_request(Request("/examplepath"))
         self.assertIsNotNone(id)
+
+    def test_register_processor(self):
+        g = GraphQLProcessor()
+        self.requestQueue.reqister_processor("/examplepath", g)
+        self.assertTrue(self.requestQueue.processors.get("/examplepath"))
+
+
+    def test_notify_request_processor(self):
+        g = GraphQLProcessor()
+
+        g.process = MagicMock(return_value = ":-)")
+
+        self.requestQueue.reqister_processor("/examplepath", g)
+
+        self.requestQueue.add_incoming_request(Request("/examplepath"))
+
+        g.process.assert_called()
