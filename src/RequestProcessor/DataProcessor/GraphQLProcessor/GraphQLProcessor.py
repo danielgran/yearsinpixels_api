f99f8631673b6fe7a9a58ca272c653ac4c0dd1ea
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-11-11 06:08:04 +0100

    Added: First working resolver to the GraphQLProcessor.py

--- src/RequestProcessor/DataProcessor/GraphQLProcessor/GraphQLProcessor.py
+++ src/RequestProcessor/DataProcessor/GraphQLProcessor/GraphQLProcessor.py
@@ -1,8 +1,9 @@
 import json
+import os
+from pathlib import Path
 
 from ariadne.constants import PLAYGROUND_HTML
-from ariadne import QueryType, gql, make_executable_schema, graphql_sync, ObjectType, load_schema_from_path, \
-    snake_case_fallback_resolvers
+from ariadne import QueryType, gql, make_executable_schema, graphql_sync, ObjectType, load_schema_from_path, snake_case_fallback_resolvers
 from ariadne.asgi import GraphQL
 
 from src.Request.Response import Response
@@ -16,18 +17,16 @@ class GraphQLProcessor(DataProcessor):
         self.mutation = ObjectType("Mutation")
         self.mutation.set_field("register", register_user)
 
+        path = Path(__file__)
+        path = path.parent.resolve()
+        path = f"{path}/schema.graphql"
+
         self.schema = make_executable_schema(
-            load_schema_from_path(r"schema.graphql")
+            load_schema_from_path(path)
         )
 
     def process(self, request):
-        # temporary
-        html = PLAYGROUND_HTML
-
         response = Response(request)
-
-        response.body = "Mock"
-        # end temporary
 
         success, result = graphql_sync(
             data=request.body,
