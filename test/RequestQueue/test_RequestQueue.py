a22e190e6c7bad64eb89398263d4a8c6b36ef805
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-11-12 07:53:19 +0100

    Refactored

--- test/RequestQueue/test_RequestQueue.py
+++ test/RequestQueue/test_RequestQueue.py
@@ -18,29 +18,26 @@ class RequestQueueTest(unittest.TestCase):
         self.assertIsNotNone(id)
 
     def test_register_processor(self):
-        g = GraphQLProcessor()
-        self.requestQueue.reqister_processor("/examplepath", g)
+        graphql_processor = GraphQLProcessor()
+        self.requestQueue.reqister_processor("/examplepath", graphql_processor)
+
         self.assertTrue(self.requestQueue.processors.get("/examplepath"))
 
     def test_notify_request_processor(self):
-        g = GraphQLProcessor()
-        g.process = MagicMock(return_value = ":-)")
-        self.requestQueue.reqister_processor("/examplepath", g)
-        self.requestQueue.add_incoming_request(Request("/examplepath"))
-        self.assertTrue(g.process.called, "Request Processor did not get called")
+        graphql_processor = GraphQLProcessor()
+        graphql_processor.process = MagicMock()
 
+        self.requestQueue.reqister_processor("/examplepath", graphql_processor)
+        self.requestQueue.add_incoming_request(Request("/examplepath"))
+        self.assertTrue(graphql_processor.process.called, "Request Processor did not get called.")
 
     def test_get_request_response(self):
-        g = GraphQLProcessor()
-        self.requestQueue.reqister_processor("/examplepath", g)
-
+        graphql_processor = GraphQLProcessor()
+        self.requestQueue.reqister_processor("/examplepath", graphql_processor)
         request = Request("/examplepath")
         request.body = {'query': 'mutation {\n    register(email: "daniel.gran")\n}'}
 
         request_id = self.requestQueue.add_incoming_request(request)
-
         response = self.requestQueue.get_response(request_id)
 
         self.assertIsNotNone(response, "Response None after processing")
-
-
